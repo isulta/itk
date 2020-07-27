e57bf94a1465318290e0c349c4400b7109063741
Imran Sultan <isultan030@gmail.com>
2020-07-27 14:37:38 -0500

    many_to_one speed-up, intersect1d/many_to_one GPU versions, misc. changes

--- itk.py
+++ itk.py
@@ -32,6 +32,28 @@ def h5_write_dict(outfile, dict, group):
         grp[k] = v
     f.close()
 
+def h5_read_files(basename=None, suffix='#', path='/', filelist=None):
+    """Read a set of HDF5 files given by a `basename` and `suffix`.
+    E.g., to read `/path/to/file#*`, `basename='/path/to/file'` and `suffix='#'`.
+    Alternatively, read all files in `filelist`.
+    Set of files are assumed to have the same keys in HDF5 `path`.
+    Returns combined dictionary of numpy arrays.
+    """
+    import h5py
+    import glob
+    assert bool(basename)^bool(filelist), "Must define basename or filelist (not both)"
+    if filelist is None:
+        filelist=glob.glob(basename+suffix+'*')
+
+    lists_all = None
+    for f in filelist:
+        dictf = h5_read_dict(f, path)
+        if lists_all is None:
+            lists_all = {k:[] for k in dictf.keys()}
+        for k in lists_all.keys():
+            lists_all[k].append(dictf[k])
+    return { k:np.concatenate(lists_all[k]) for k in lists_all.keys() }
+
 def gio_read_dict(f, cols):
     """Read cols of GenericIO file f and return dictionary of numpy arrays."""
     import genericio as gio
@@ -42,18 +64,24 @@ def pickle_save_dict(f, d):
     import pickle
     pickle.dump( d, open( f, 'wb' ) )
 
-def loadpickle(f):
-    """Returns pickled object in f."""
-    import pickle
-    return pickle.load( open( f, 'rb' ) )
-
-def hist(vals, bins=500, normed=False, plotFlag=True, label='', alpha=1, range=None, normScalar=1, normCnts=False, normBinsize=False, normLogCnts=False, plotOptions='.'):
+def loadpickle(f, python2topython3=False):
+    """Returns pickled object in f.
+    python2topython3: Reads in file originally pickled with Python2 into Python3 https://rebeccabilbro.github.io/convert-py2-pickles-to-py3/
     """
+    import pickle
+    if python2topython3:
+        return pickle.load( open(f, 'rb'), encoding='latin1' ) 
+    else:
+        return pickle.load( open(f, 'rb') )
+
+def hist(vals, bins=500, normed=False, plotFlag=True, label='', alpha=1, range=None, normScalar=1, normCnts=False, normBinsize=False, normLogCnts=False, plotOptions='.', ax=None, retEbars=False):
+    """
+    DOCUMENTATION OUT OF DATA TODO: fix documentation
     Returns histogram of vals in (default 500) bins.
-    
+
     Parameters
     ----------
-    vals : np 1d array 
+    vals : np 1d array
         Data points
     normed : bool
         If true, normalizes by counts (if `normCnts`), bin size (if `normBinsize`), scalar (if `normScalar` given), log10(cnts) (if `normLogCnts`)
@@ -61,17 +89,28 @@ def hist(vals, bins=500, normed=False, plotFlag=True, label='', alpha=1, range=N
         If true, plots histogram with `label` and `alpha`.
     range : tuple
         If given, returns hist of vals within range; if not, uses all vals.
-    
+
     Returns
     -------
-    xarr : np 1d array 
+    xarr : np 1d array
         Array of len(cnts) of bin midpoints
-    cnts : np 1d array 
+    cnts : np 1d array
         Bin counts (normalized if `normed`)
     """
     cnts, bedg = np.histogram(vals, bins=bins, range=range)
     xarr = bedg[:-1] + (bedg[1:] - bedg[:-1])/2.
-    
+
+    #Ebars calculation
+    if retEbars:
+        assert (not normCnts and normLogCnts)
+        ### error propogation method for log10(cnts/C) ###
+        ebars_log = 1./np.log(10) * np.sqrt(cnts)/cnts
+        ### log(x+-dx) method for log10(cnts/C) ###
+        # eb_lower = np.log10(np.true_divide(cnts, cnts-np.sqrt(cnts)))
+        # eb_upper = -np.log10(np.true_divide(cnts, cnts+np.sqrt(cnts)))
+        # ebars_log = (eb_lower, eb_upper)
+
+        ebars = np.true_divide(np.sqrt(cnts), (bedg[1:] - bedg[:-1])*normScalar*1.0)
     # Normalization
     if normed:
         if normCnts:
@@ -84,24 +123,124 @@ def hist(vals, bins=500, normed=False, plotFlag=True, label='', alpha=1, range=N
              cnts = np.log10(cnts)
 
     if plotFlag:
-        plt.plot(xarr, cnts, plotOptions, ms=2,label=label, alpha=alpha)
-    return (xarr, cnts)
+        if ax:
+            #ax.plot(xarr, cnts, plotOptions, ms=2,label=label, alpha=alpha)
+            ax.plot(xarr, cnts, alpha=alpha, label=label)
+        else:
+            #plt.plot(xarr, cnts, plotOptions, ms=2,label=label, alpha=alpha)
+            plt.plot(xarr, cnts, alpha=alpha, label=label)
+    if retEbars:
+        return (xarr, cnts, ebars, ebars_log)
+    else:
+        return (xarr, cnts)
 
 def periodic_bcs(x, x_ref, boxsize):
     """Returns `x` (np array or scalar) adjusted to have shortest distance to `x_ref` (np array (only permitted if `x` is array) or scalar) according to PBCs of length boxsize."""
     return x + boxsize*((x-x_ref)<-(boxsize/2)) + -boxsize*((x-x_ref)>(boxsize/2))
 
-def many_to_one(x1, x0):
-    """Performs a many-to-one matching from `x1` to `x0`, and returns array of indices of `x0` such that x0[idx] is x1.
+def many_to_one_OLD(x1, x0, verbose=False, assert_x0_unique=True, assert_x1_in_x0=True):
+    """Old version. Performs a many-to-one matching from `x1` to `x0`, and returns array of indices of `x0` such that x0[idx] is x1.
     Every element of `x1` must be in `x0`, and `x0` must be unique.
     """
-    assert len(np.unique(x0))==len(x0), 'Elements of x0 are not unique.'
-    assert np.all(np.isin(x1,x0)), "Elements of x1 exist that are not in x0."
+    if assert_x0_unique:
+        assert len(np.unique(x0))==len(x0), 'Elements of x0 are not unique.'
+        if verbose:
+            print('Assert 1: Elements of x0 are unique.')
+    if assert_x1_in_x0:
+        assert np.all(np.isin(x1,x0)), "Elements of x1 exist that are not in x0."
+        if verbose:
+            print('Assert 2: All elements of x1 are found in x0.')
 
     # Match x1 elements with x0.
     _, _, idx4 = np.intersect1d(x1, x0, return_indices=True, assume_unique=False)
+    if verbose:
+        print('Intersect x1 and x0.')
 
     # Unique x1 elements with inverse indices.
     _, idx_inv = np.unique(x1, return_inverse=True)
+    if verbose:
+        print('Unique x1')
 
     return idx4[idx_inv]
+
+def many_to_one(x1arr, x0arr, verbose=False, assert_x0_unique=True, assert_x1_in_x0=True):
+    """Performs a many-to-one matching from `x1arr` to `x0arr`, and returns array of indices of `x0arr` such that x0arr[idx] is x1arr.
+    Every element of `x1arr` must be in `x0arr`, and `x0arr` must be unique.
+    Based on np.intersect1d.
+    """
+    x1, ind1, inv1 = np.unique(x1arr, return_index=True, return_inverse=True)
+    x0, ind2 = np.unique(x0arr, return_index=True)
+    if assert_x0_unique:
+        assert len(x0)==len(x0arr), 'Elements of x0arr are not unique.'
+        if verbose:
+            print('Assert 1: Elements of x0arr are unique.')
+    if assert_x1_in_x0:
+        assert np.all(np.isin(x1arr,x0arr)), "Elements of x1arr exist that are not in x0arr."
+        if verbose:
+            print('Assert 2: All elements of x1arr are found in x0arr.')
+    aux = np.concatenate((x1, x0))
+    aux_sort_indices = np.argsort(aux, kind='mergesort')
+    aux = aux[aux_sort_indices]
+    mask = aux[1:] == aux[:-1]
+    x0_indices = ind2[ aux_sort_indices[1:][mask] - x1.size ]
+    return x0_indices[inv1]
+
+def reldif(x1,x2):
+    return np.abs(x1-x2)/((x1+x2)/2)
+
+def nratioerr(n, nerr, nfid, nfiderr):
+    return np.sqrt( (nerr/nfid)**2 + (n*nfiderr/nfid**2)**2 )
+
+def intersect1d_GPU(ar1, ar2, assume_unique=False, return_indices=False):
+    '''Based on np.intersect1d. Special fn for LJ cc gen'''
+    import cupy as cp
+    assert (not assume_unique) and return_indices
+
+    ar1 = cp.asarray(ar1)                           # ar1 to GPU
+    ar1, ind1 = cp.unique(ar1, return_index=True)   # ar1, ind1 on GPU
+    ar1 = cp.asnumpy(ar1)                           # ar1 to Host
+    ind1 = cp.asnumpy(ind1)                         # ind1 to Host
+
+    ar2 = cp.asarray(ar2)                           # ar2 to GPU
+    ar2, ind2 = cp.unique(ar2, return_index=True)   # ar2, ind2 on GPU
+    ar2 = cp.asnumpy(ar2)                           # ar2 to Host
+    ind2 = cp.asnumpy(ind2)                         # ind2 to Host
+
+    aux = np.concatenate((ar1, ar2))                        # aux on Host
+    aux_sort_indices = np.argsort(aux, kind='mergesort')    # aux_sort_indices on Host
+    aux = aux[aux_sort_indices]
+
+    mask = aux[1:] == aux[:-1]                          # mask on Host
+    int1d = aux[:-1][mask]                              # int1d on Host
+
+    ar1_indices = aux_sort_indices[:-1][mask]           # ar1_indices on Host
+    ar2_indices = aux_sort_indices[1:][mask] - ar1.size # ar2_indices on Host
+
+    ar1_indices = ind1[ar1_indices]
+    ar2_indices = ind2[ar2_indices]
+
+    return int1d, ar1_indices, ar2_indices # return on Host
+
+def many_to_one_GPU(ar1, ar2):
+    '''Based on np.intersect1d. Special fn for LJ cc gen'''
+    import cupy as cp
+    ar1 = cp.asarray(ar1)                                                       # ar1 to GPU
+    ar1, ind1, inv1 = cp.unique(ar1, return_index=True, return_inverse=True)    # ar1, ind1, inv1 on GPU
+    ar1 = cp.asnumpy(ar1)                                                       # ar1 to Host
+    ind1 = cp.asnumpy(ind1)                                                     # ind1 to Host
+    inv1 = cp.asnumpy(inv1)                                                     # inv1 to Host
+
+    ar2 = cp.asarray(ar2)                                                       # ar2 to GPU
+    ar2, ind2 = cp.unique(ar2, return_index=True)                               # ar2, ind2 on GPU
+    ar2 = cp.asnumpy(ar2)                                                       # ar2 to Host
+    ind2 = cp.asnumpy(ind2)                                                     # ind2 to Host
+
+    aux = np.concatenate((ar1, ar2))                                            # aux on Host
+    aux_sort_indices = np.argsort(aux, kind='mergesort')                        # aux_sort_indices on Host
+    aux = aux[aux_sort_indices]
+
+    mask = aux[1:] == aux[:-1]                                                  # mask on Host
+
+    ar2_indices = ind2[ aux_sort_indices[1:][mask] - ar1.size ]                 # ar2_indices on Host
+
+    return ar2_indices[inv1]                                                    # return on Host
